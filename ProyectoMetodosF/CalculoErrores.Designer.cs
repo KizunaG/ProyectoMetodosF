00dbde0596868498e1037cb6a0d348a81d1a588a
KizunaG <hecto@LAPTOP-IUQ31NPB>
2024-05-27 16:14:47 -0600

    Se modificó y se agregó funcionando el Metodo de Newton

--- ProyectoMetodosF/CalculoErrores.Designer.cs
+++ ProyectoMetodosF/CalculoErrores.Designer.cs
@@ -28,155 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.longitudPuenteVerdaderaTextBox = new System.Windows.Forms.TextBox();
-            this.longitudPuenteMedidaTextBox = new System.Windows.Forms.TextBox();
-            this.calcularButton = new System.Windows.Forms.Button();
-            this.mensajeLabel = new System.Windows.Forms.Label();
-            this.errorPuenteLabel = new System.Windows.Forms.Label();
-            this.errorRelativoPuenteLabel = new System.Windows.Forms.Label();
-            this.longitudRemacheVerdaderaTextBox = new System.Windows.Forms.TextBox();
-            this.longitudRemacheMedidaTextBox = new System.Windows.Forms.TextBox();
-            this.errorRemacheLabel = new System.Windows.Forms.Label();
-            this.errorRelativoRemacheLabel = new System.Windows.Forms.Label();
-            this.btnclear = new System.Windows.Forms.Button();
+            this.txtFuncion = new System.Windows.Forms.TextBox();
+            this.txtx0 = new System.Windows.Forms.TextBox();
+            this.txtTolerancia = new System.Windows.Forms.TextBox();
+            this.txtMaxiter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnPdf = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
+            this.dataGridViewResultadoNewtonRaphson = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultadoNewtonRaphson)).BeginInit();
             this.SuspendLayout();
             // 
-            // longitudPuenteVerdaderaTextBox
+            // txtFuncion
             // 
-            this.longitudPuenteVerdaderaTextBox.Location = new System.Drawing.Point(76, 79);
-            this.longitudPuenteVerdaderaTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.longitudPuenteVerdaderaTextBox.Name = "longitudPuenteVerdaderaTextBox";
-            this.longitudPuenteVerdaderaTextBox.Size = new System.Drawing.Size(132, 22);
-            this.longitudPuenteVerdaderaTextBox.TabIndex = 0;
+            this.txtFuncion.Location = new System.Drawing.Point(57, 44);
+            this.txtFuncion.Name = "txtFuncion";
+            this.txtFuncion.Size = new System.Drawing.Size(100, 20);
+            this.txtFuncion.TabIndex = 0;
             // 
-            // longitudPuenteMedidaTextBox
+            // txtx0
             // 
-            this.longitudPuenteMedidaTextBox.Location = new System.Drawing.Point(263, 79);
-            this.longitudPuenteMedidaTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.longitudPuenteMedidaTextBox.Name = "longitudPuenteMedidaTextBox";
-            this.longitudPuenteMedidaTextBox.Size = new System.Drawing.Size(132, 22);
-            this.longitudPuenteMedidaTextBox.TabIndex = 1;
+            this.txtx0.Location = new System.Drawing.Point(163, 44);
+            this.txtx0.Name = "txtx0";
+            this.txtx0.Size = new System.Drawing.Size(100, 20);
+            this.txtx0.TabIndex = 1;
             // 
-            // calcularButton
+            // txtTolerancia
             // 
-            this.calcularButton.Location = new System.Drawing.Point(465, 150);
-            this.calcularButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.calcularButton.Name = "calcularButton";
-            this.calcularButton.Size = new System.Drawing.Size(100, 28);
-            this.calcularButton.TabIndex = 2;
-            this.calcularButton.Text = "Calcular";
-            this.calcularButton.UseVisualStyleBackColor = true;
-            this.calcularButton.Click += new System.EventHandler(this.calcularButton_Click);
+            this.txtTolerancia.Location = new System.Drawing.Point(269, 44);
+            this.txtTolerancia.Name = "txtTolerancia";
+            this.txtTolerancia.Size = new System.Drawing.Size(100, 20);
+            this.txtTolerancia.TabIndex = 2;
             // 
-            // mensajeLabel
+            // txtMaxiter
             // 
-            this.mensajeLabel.AutoSize = true;
-            this.mensajeLabel.Location = new System.Drawing.Point(203, 219);
-            this.mensajeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.mensajeLabel.Name = "mensajeLabel";
-            this.mensajeLabel.Size = new System.Drawing.Size(44, 16);
-            this.mensajeLabel.TabIndex = 3;
-            this.mensajeLabel.Text = "label1";
+            this.txtMaxiter.Location = new System.Drawing.Point(375, 44);
+            this.txtMaxiter.Name = "txtMaxiter";
+            this.txtMaxiter.Size = new System.Drawing.Size(100, 20);
+            this.txtMaxiter.TabIndex = 3;
             // 
-            // errorPuenteLabel
+            // btnCalculate
             // 
-            this.errorPuenteLabel.AutoSize = true;
-            this.errorPuenteLabel.Location = new System.Drawing.Point(351, 219);
-            this.errorPuenteLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.errorPuenteLabel.Name = "errorPuenteLabel";
-            this.errorPuenteLabel.Size = new System.Drawing.Size(44, 16);
-            this.errorPuenteLabel.TabIndex = 4;
-            this.errorPuenteLabel.Text = "label1";
+            this.btnCalculate.Location = new System.Drawing.Point(146, 123);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calcular";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
-            // errorRelativoPuenteLabel
+            // btnPdf
             // 
-            this.errorRelativoPuenteLabel.AutoSize = true;
-            this.errorRelativoPuenteLabel.Location = new System.Drawing.Point(351, 266);
-            this.errorRelativoPuenteLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.errorRelativoPuenteLabel.Name = "errorRelativoPuenteLabel";
-            this.errorRelativoPuenteLabel.Size = new System.Drawing.Size(44, 16);
-            this.errorRelativoPuenteLabel.TabIndex = 5;
-            this.errorRelativoPuenteLabel.Text = "label2";
-            // 
-            // longitudRemacheVerdaderaTextBox
-            // 
-            this.longitudRemacheVerdaderaTextBox.Location = new System.Drawing.Point(465, 79);
-            this.longitudRemacheVerdaderaTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.longitudRemacheVerdaderaTextBox.Name = "longitudRemacheVerdaderaTextBox";
-            this.longitudRemacheVerdaderaTextBox.Size = new System.Drawing.Size(132, 22);
-            this.longitudRemacheVerdaderaTextBox.TabIndex = 6;
-            // 
-            // longitudRemacheMedidaTextBox
-            // 
-            this.longitudRemacheMedidaTextBox.Location = new System.Drawing.Point(670, 79);
-            this.longitudRemacheMedidaTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.longitudRemacheMedidaTextBox.Name = "longitudRemacheMedidaTextBox";
-            this.longitudRemacheMedidaTextBox.Size = new System.Drawing.Size(132, 22);
-            this.longitudRemacheMedidaTextBox.TabIndex = 7;
-            // 
-            // errorRemacheLabel
-            // 
-            this.errorRemacheLabel.AutoSize = true;
-            this.errorRemacheLabel.Location = new System.Drawing.Point(732, 219);
-            this.errorRemacheLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.errorRemacheLabel.Name = "errorRemacheLabel";
-            this.errorRemacheLabel.Size = new System.Drawing.Size(44, 16);
-            this.errorRemacheLabel.TabIndex = 8;
-            this.errorRemacheLabel.Text = "label1";
-            // 
-            // errorRelativoRemacheLabel
-            // 
-            this.errorRelativoRemacheLabel.AutoSize = true;
-            this.errorRelativoRemacheLabel.Location = new System.Drawing.Point(732, 266);
-            this.errorRelativoRemacheLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.errorRelativoRemacheLabel.Name = "errorRelativoRemacheLabel";
-            this.errorRelativoRemacheLabel.Size = new System.Drawing.Size(44, 16);
-            this.errorRelativoRemacheLabel.TabIndex = 9;
-            this.errorRelativoRemacheLabel.Text = "label2";
-            // 
-            // btnclear
-            // 
-            this.btnclear.Location = new System.Drawing.Point(607, 150);
-            this.btnclear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnclear.Name = "btnclear";
-            this.btnclear.Size = new System.Drawing.Size(100, 28);
-            this.btnclear.TabIndex = 10;
-            this.btnclear.Text = "Limpiar";
-            this.btnclear.UseVisualStyleBackColor = true;
-            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            this.btnPdf.Location = new System.Drawing.Point(249, 123);
+            this.btnPdf.Name = "btnPdf";
+            this.btnPdf.Size = new System.Drawing.Size(75, 23);
+            this.btnPdf.TabIndex = 5;
+            this.btnPdf.Text = "PDF";
+            this.btnPdf.UseVisualStyleBackColor = true;
+            this.btnPdf.Click += new System.EventHandler(this.btnPdf_Click);
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(108, 335);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClose.Location = new System.Drawing.Point(40, 261);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(100, 28);
-            this.btnClose.TabIndex = 11;
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
             this.btnClose.Text = "Cerrar";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // dataGridViewResultadoNewtonRaphson
+            // 
+            this.dataGridViewResultadoNewtonRaphson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResultadoNewtonRaphson.Location = new System.Drawing.Point(375, 110);
+            this.dataGridViewResultadoNewtonRaphson.Name = "dataGridViewResultadoNewtonRaphson";
+            this.dataGridViewResultadoNewtonRaphson.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewResultadoNewtonRaphson.TabIndex = 7;
+            // 
             // MetodoNewton
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 507);
+            this.ClientSize = new System.Drawing.Size(637, 412);
+            this.Controls.Add(this.dataGridViewResultadoNewtonRaphson);
             this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.btnclear);
-            this.Controls.Add(this.errorRelativoRemacheLabel);
-            this.Controls.Add(this.errorRemacheLabel);
-            this.Controls.Add(this.longitudRemacheMedidaTextBox);
-            this.Controls.Add(this.longitudRemacheVerdaderaTextBox);
-            this.Controls.Add(this.errorRelativoPuenteLabel);
-            this.Controls.Add(this.errorPuenteLabel);
-            this.Controls.Add(this.mensajeLabel);
-            this.Controls.Add(this.calcularButton);
-            this.Controls.Add(this.longitudPuenteMedidaTextBox);
-            this.Controls.Add(this.longitudPuenteVerdaderaTextBox);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.btnPdf);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtMaxiter);
+            this.Controls.Add(this.txtTolerancia);
+            this.Controls.Add(this.txtx0);
+            this.Controls.Add(this.txtFuncion);
             this.Name = "MetodoNewton";
             this.Text = "MetodoNewton";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultadoNewtonRaphson)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,17 +128,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox longitudPuenteVerdaderaTextBox;
-        private System.Windows.Forms.TextBox longitudPuenteMedidaTextBox;
-        private System.Windows.Forms.Button calcularButton;
-        private System.Windows.Forms.Label mensajeLabel;
-        private System.Windows.Forms.Label errorPuenteLabel;
-        private System.Windows.Forms.Label errorRelativoPuenteLabel;
-        private System.Windows.Forms.TextBox longitudRemacheVerdaderaTextBox;
-        private System.Windows.Forms.TextBox longitudRemacheMedidaTextBox;
-        private System.Windows.Forms.Label errorRemacheLabel;
-        private System.Windows.Forms.Label errorRelativoRemacheLabel;
-        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.TextBox txtFuncion;
+        private System.Windows.Forms.TextBox txtx0;
+        private System.Windows.Forms.TextBox txtTolerancia;
+        private System.Windows.Forms.TextBox txtMaxiter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnPdf;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.DataGridView dataGridViewResultadoNewtonRaphson;
     }
 }
--- ProyectoMetodosF/CalculoErrores.cs
+++ ProyectoMetodosF/CalculoErrores.cs
@@ -1,69 +1,216 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using NCalc;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace ProyectoMetodosF
 {
     public partial class MetodoNewton : Form
     {
+        private string connectionString = "Data source=LAPTOP-IUQ31NPB\\SQLEXPRESS; Initial Catalog=ProyectoMetodos; Integrated security=true;";
         public MetodoNewton()
         {
             InitializeComponent();
         }
 
-        private void calcularButton_Click(object sender, EventArgs e)
+        private void btnCalculate_Click(object sender, EventArgs e)
         {
             try
             {
-                // Obtener los valores ingresados por el usuario
-                double longitudPuenteVerdadera = double.Parse(longitudPuenteVerdaderaTextBox.Text);
-                double longitudPuenteMedida = double.Parse(longitudPuenteMedidaTextBox.Text);
-                double longitudRemacheVerdadera = double.Parse(longitudRemacheVerdaderaTextBox.Text);
-                double longitudRemacheMedida = double.Parse(longitudRemacheMedidaTextBox.Text);
+                string funcion = txtFuncion.Text;
 
-                // Calcular los errores para el puente
-                double errorVerdaderoPuente = longitudPuenteVerdadera - longitudPuenteMedida;
-                double errorRelativoPuente = (errorVerdaderoPuente / longitudPuenteVerdadera) * 100;
+                if (!double.TryParse(txtx0.Text, out double x0))
+                {
+                    MessageBox.Show("Valor de x0 no válido");
+                    return;
+                }
 
-                // Calcular los errores para el remache
-                double errorVerdaderoRemache = longitudRemacheVerdadera - longitudRemacheMedida;
-                double errorRelativoRemache = (errorVerdaderoRemache / longitudRemacheVerdadera) * 100;
+                if (!double.TryParse(txtTolerancia.Text, out double tolerancia))
+                {
+                    MessageBox.Show("Valor de tolerancia no válido");
+                    return;
+                }
 
-                // Mostrar los resultados
-                errorPuenteLabel.Text = "Error verdadero del puente: " + errorVerdaderoPuente.ToString("F2") + " cm";
-                errorRelativoPuenteLabel.Text = "Error relativo porcentual del puente: " + errorRelativoPuente.ToString("F2") + "%";
-                errorRemacheLabel.Text = "Error verdadero del remache: " + errorVerdaderoRemache.ToString("F2") + " cm";
-                errorRelativoRemacheLabel.Text = "Error relativo porcentual del remache: " + errorRelativoRemache.ToString("F2") + "%";
-                mensajeLabel.Text = ""; // Limpiar cualquier mensaje de error previo
+                if (!int.TryParse(txtMaxiter.Text, out int maxIter))
+                {
+                    MessageBox.Show("Valor de iteraciones máximas no válido");
+                    return;
+                }
+
+                if (!ValidarFuncion(funcion))
+                {
+                    MessageBox.Show("Función no válida. Asegúrate de que la función sea correcta.");
+                    return;
+                }
+
+                var iteraciones = new List<(int iteracion, double xr, double error)>();
+
+                for (int i = 0; i < maxIter; i++)
+                {
+                    double fx = EvaluarFuncion(funcion, x0);
+                    double dfx = EvaluarDerivada(funcion, x0);
+
+                    if (dfx == 0)
+                    {
+                        MessageBox.Show("La derivada es cero, no se puede continuar.");
+                        return;
+                    }
+
+                    double xr = x0 - fx / dfx;
+                    double error = Math.Abs(xr - x0);
+
+                    iteraciones.Add((i + 1, xr, error));
+
+                    if (error < tolerancia)
+                        break;
+
+                    x0 = xr;
+                }
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    foreach (var iter in iteraciones)
+                    {
+                        string query = "INSERT INTO MetodoNewtonRaphson(calculo_id, iteracion, xr, error) VALUES (@calculo_id, @iteracion, @xr, @error)";
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@calculo_id", 1);
+                            command.Parameters.AddWithValue("@iteracion", iter.iteracion);
+                            command.Parameters.AddWithValue("@xr", iter.xr);
+                            command.Parameters.AddWithValue("@error", iter.error);
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                CargarDatosDesdeSQL();
             }
             catch (Exception ex)
             {
-                mensajeLabel.Text = "Error: " + ex.Message;
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private double EvaluarFuncion(string funcion, double x)
+        {
+            try
+            {
+                var expression = new Expression(funcion);
+                expression.Parameters["x"] = x;
+                return Convert.ToDouble(expression.Evaluate());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al evaluar la función: " + ex.Message);
+                throw;
+            }
+        }
+
+        private double EvaluarDerivada(string funcion, double x)
+        {
+            try
+            {
+                double h = 1e-5; // Pequeño incremento
+                var expression = new Expression(funcion);
+
+                expression.Parameters["x"] = x + h;
+                double fxh = Convert.ToDouble(expression.Evaluate());
+
+                expression.Parameters["x"] = x;
+                double fx = Convert.ToDouble(expression.Evaluate());
+
+                return (fxh - fx) / h;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al evaluar la derivada: " + ex.Message);
+                throw;
+            }
+        }
+
+        private bool ValidarFuncion(string funcion)
+        {
+            try
+            {
+                var expression = new Expression(funcion);
+                expression.Parameters["x"] = 1;
+                var result = expression.Evaluate();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void CargarDatosDesdeSQL()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT iteracion, xr, error FROM MetodoNewtonRaphson";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridViewResultadoNewtonRaphson.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar datos: " + ex.Message);
+            }
+        }
+
+        private void btnPdf_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files|*.pdf";
+            saveFileDialog.Title = "Save a PDF File";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    Document pdfDoc = new Document(PageSize.A4);
+                    PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+
+                    PdfPTable table = new PdfPTable(dataGridViewResultadoNewtonRaphson.Columns.Count);
+
+                    foreach (DataGridViewColumn column in dataGridViewResultadoNewtonRaphson.Columns)
+                    {
+                        table.AddCell(new Phrase(column.HeaderText));
+                    }
+
+                    foreach (DataGridViewRow row in dataGridViewResultadoNewtonRaphson.Rows)
+                    {
+                        if (row.IsNewRow) continue;
+                        foreach (DataGridViewCell cell in row.Cells)
+                        {
+                            table.AddCell(new Phrase(cell.Value?.ToString() ?? ""));
+                        }
+                    }
+
+                    pdfDoc.Add(table);
+                    pdfDoc.Close();
+                }
             }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void btnclear_Click(object sender, EventArgs e)
-        {
-            longitudPuenteVerdaderaTextBox.Text = "";
-            longitudPuenteMedidaTextBox.Text = "";
-            longitudRemacheVerdaderaTextBox.Text = "";
-            longitudRemacheMedidaTextBox.Text = "";
-            errorPuenteLabel.Text = "";
-            errorRelativoPuenteLabel.Text = "";
-            errorRemacheLabel.Text = "";
-            errorRelativoRemacheLabel.Text = "";
         }
     }
 }
