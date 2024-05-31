7a481f64e29435c04c3777c16e8592ebf07611b9
KizunaG <hecto@LAPTOP-IUQ31NPB>
2024-05-31 17:46:04 -0600

    Se agregó la tabla para los integrantes

--- ProyectoMetodosF/Integrantes.cs
+++ ProyectoMetodosF/Integrantes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,18 +10,41 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProyectoMetodosF
-{
+{ 
     public partial class Integrantes : Form
     {
+        private string connectionString = "Data source=LAPTOP-IUQ31NPB\\SQLEXPRESS; Initial Catalog=ProyectoMetodos; Integrated security=true;";
         public Integrantes()
         {
             InitializeComponent();
         }
-
         private void Integrantes_Load(object sender, EventArgs e)
         {
-
+            CargarDatosDesdeSQL();
         }
+        private void CargarDatosDesdeSQL()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM IntegrantesTrabajo";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        dataGridView2.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            }
+        }
+
 
         private void label11_Click(object sender, EventArgs e)
         {
